2e567f4f85008243893a96ac6c8061e672d02abe
guillaume riviere <guillaume.riv@gmail.com>
2023-08-16 23:56:57 +0200

    Put back the log and the duplicate folder in the root folder

--- gtakeout_media_fixer.py
+++ gtakeout_media_fixer.py
@@ -47,8 +47,8 @@ class GTakeoutMediaFixer:
                 break
             elif event == "Fix":
                 self._root_path = Path(values["-IN2-"])
-                self._duplicates_path = self._root_path.parent / 'duplicates'
-                logging.basicConfig(filename=self._root_path.parent / 'fix_info.log', encoding='utf-8', level=logging.INFO)
+                self._duplicates_path = self._root_path / '**DUPLICATES**'
+                logging.basicConfig(filename=self._root_path / 'fix_info.log', encoding='utf-8', level=logging.INFO)
                 self._conversion_path(self._root_path, dry_run=True)
                 self._conversion_path(self._root_path)
                 self._window['-PROGRESS_LABEL-'].update('100%')
